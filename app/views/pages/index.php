07e933157980758f74b303e43b442d0472b2022f
Ed Werner <android.devops@gmail.com>
2019-04-22 13:49:26 -0400

    updates index page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <h1><?php echo $data['title']; ?></h1>
 <div class='video-container'>
 	<div id="player"></div>
 </div>
@@ -27,45 +29,9 @@
 			'onReady': onPlayerReady,
 		}
 	});
-	}
-function onPlayerReady(event) {
-event.target.mute();
 }
-</script>
-
-<?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1><?php echo $data['title']; ?></h1><div class='video-container'
-	<div id="player"></div>
-</div>
-<script>
-	// Load the IFrame Player API code asynchronously.
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	// Replace the 'ytplayer' element with an <iframe> and
-	// YouTube player after the API code downloads.
-	var player;
-	function onYouTubePlayerAPIReady() {
-		player = new YT.Player('player', {
-		height: '100%',
-		width: '100%',
-		videoId: 'XB2g7-HgE_g',
-		playerVars: {
-			autoplay: 1,
-			loop: 1,
-			controls: 0,
-			rel: 0,
-			showinfo: 0,
-			playlist: 'XB2g7-HgE_g'
-		},
-		events: {
-			'onReady': onPlayerReady,
-		}
-	});
-	}
 function onPlayerReady(event) {
-event.target.mute();
+	event.target.mute();
 }
 </script>
 
